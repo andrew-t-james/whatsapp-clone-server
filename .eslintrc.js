31d4818aee5924ef61d04721d5103ea36fb89e13
Andrew James <ajames20@gmail.com>
2019-11-23 10:51:29 -0600

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  settings: {},
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
+  rules: {
+    'react/no-unescaped-entities': 'off'
+  },
+  overrides: []
+};
