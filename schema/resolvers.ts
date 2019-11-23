28435b5d72c8a7498655523cf9acad3be3fc8457
Andrew James <ajames20@gmail.com>
2019-11-23 11:58:05 -0600

    Add query and to resolvers

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,12 +1,30 @@
 import { DateTimeResolver, URLResolver } from 'graphql-scalars'
 
-import { chats } from '../db'
+import { chats, messages } from '../db'
+
+interface Message {
+  id: string
+  content: string
+  createdAt: Date
+}
+
+interface Chat {
+  id: string
+  name: string
+  picture: string
+  lastMessage: string
+}
 
 const resolvers = {
   Date: DateTimeResolver,
   URL: URLResolver,
+  Chat: {
+    lastMessage(chat: Chat): Message | undefined {
+      return messages.find(m => m.id === chat.lastMessage)
+    }
+  },
   Query: {
-    chats() {
+    chats(): Chat[] {
       return chats
     }
   }
