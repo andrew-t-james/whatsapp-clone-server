6de9e323df8d7b27f87cd0dcc0c5a320d617ae10
Andrew James <ajames20@gmail.com>
2019-11-28 13:28:50 -0600

    Add mutation for a message

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -32,6 +32,30 @@ const resolvers = {
       return messages.filter(m => chat.messages.includes(m.id))
     }
   },
+
+  Mutation: {
+    addMessage(root: object, { id, content }: Message): Message | null {
+      const chatIndex = chats.findIndex(c => c.id === id)
+      if (chatIndex === -1) return null
+      const chat = chats[chatIndex]
+      const messagesIds = messages.map(currentMessage =>
+        Number(currentMessage.id)
+      )
+      const messageId = String(Math.max(...messagesIds) + 1)
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content
+      }
+      messages.push(message)
+      chat.messages.push(messageId)
+      // The chat will appear at the top of the ChatsList component
+      chats.splice(chatIndex, 1)
+      chats.unshift(chat)
+      return message
+    }
+  },
+
   Query: {
     chats(): Chats[] {
       return chats
@@ -41,4 +65,5 @@ const resolvers = {
     }
   }
 }
+
 export default resolvers
